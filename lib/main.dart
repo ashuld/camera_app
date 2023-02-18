baaf0b0eb965e0c93eac4884744ef3036a4c5996
Ashish Wilson <ashish.wils@gmail.com>
2023-02-18 11:40:07 +0530

    final commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:camera_app/screens/galleryscreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      debugShowCheckedModeBanner: false,
+      home: const ScreenGallery(),
+    );
+  }
+}
